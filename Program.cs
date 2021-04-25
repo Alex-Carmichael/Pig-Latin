20605f7f3e936484dff4401d188fb0fd55469e0a
Alex-Carmichael <alexcarmichael857@gmail.com>
2021-04-25 10:48:07 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pig_Latin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your word: ");
+            string word = Console.ReadLine();
+
+            word = word + "-" + word[0] + "ay";
+            word = word.Substring(1);
+            Console.Write(word);
+            Console.ReadKey();
+        }
+    }
+}
